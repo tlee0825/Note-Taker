16cf7558cee0c1b6341593e463fd20a9a171158a
Teshera Lee <tcasey0825@gmail.com>
2021-01-23 21:32:14 -0500

    Added notes html
    added notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,41 +1,50 @@
-const fs= require ("fs")
+const fs = require("fs")
 
-let notesData = require("../data/notesData");
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = function (app) {
 
-  app.get("/api/notes", function(req, res) {
+  app.get("/api/notes", function (req, res) {
     let notes;
     fs.readFile("../db/db.json", (err, data) => {
-        if (err) throw err;
-        notes = JSON.parse(data);
-        //console.log(notes)
-        return res.json(notes)
+      if (err) throw err;
+      notes = JSON.parse(data);
+      //console.log(notes)
+      return res.json(notes)
     });
-});
+  });
 
-app.post("/api/notes", function(req, res) {
-  let newNote = req.body;
-  let noteID = uuidv4();
-  //console.log(newNote);
-  Object.assign(newNote, {id: noteID});
-  //console.log(newNote);
-  fs.readFile("../db/db.json", (err, data) => {
+  app.post("/api/notes", function (req, res) {
+    let newNote = req.body;
+    let noteID = uuidv4();
+    //console.log(newNote);
+    Object.assign(newNote, { id: noteID });
+    //console.log(newNote);
+    fs.readFile("../db/db.json", (err, data) => {
       if (err) throw err;
       let database = JSON.parse(data);
       database.push(newNote);
       fs.writeFile("../db/db.json", JSON.stringify(database), (err) => {
-          if (err) throw err;
-          console.log("New note has been saved");
+        if (err) throw err;
+        console.log("New note has been saved");
       });
       return res.json(database);
+    });
   });
-});
-  app.post("/api/clear", function (req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
-  });
-};
+  app.delete("/api/notes/:id", function (req, res) {
+    let id = req.params.id
+    fs.readFile("../db/db.json", (err, data) => {
+      if (err) throw err;
+      let database = JSON.parse(data);
+      for (var i = 0; i < database.length; i++) {
+        if (database[i].id === id) {
+          database.splice(i, 1)
+        }
+      }
+      fs.writeFile("../db/db.json", JSON.stringify(database), (err) => {
+        if (err) throw err;
+      })
+      return res.json(database)
+    })
+  })
+}
--- server.js
+++ server.js
@@ -1,15 +1,83 @@
-var express = require("express");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
 var app = express();
-
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-require("./routes/apiRoutes.js")(app);
-require("./routes/htmlRoutes.js")(app);
+// Serves html files
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// Reads jb.json file and returns saved notes (unfinished)
+app.get("/api/notes", function(req, res) {
+    let notes;
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        notes = JSON.parse(data);
+        //console.log(notes)
+        return res.json(notes)
+    });
+});
+
+// Posts new notes to database (works)
+app.post("/api/notes", function(req, res) {
+    let newNote = req.body;
+    let noteID = uuidv4();
+    //console.log(newNote);
+    Object.assign(newNote, {id: noteID});
+    //console.log(newNote);
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        let database = JSON.parse(data);
+        database.push(newNote);
+        
+        fs.writeFile("./db/db.json", JSON.stringify(database), (err) => {
+            if (err) throw err;
+            console.log("New note has been saved");
+        });
+
+        return res.json(database);
+    });
+
+    
+});
+
+//Deletes notes (unfinished)
+app.delete("/api/notes/:id", function(req, res) {
+    var noteID = req.params.id;
+    console.log(noteID);
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        let database = JSON.parse(data);
+        for (var i = 0; i < database.length; i++) {
+            if (database[i].id === noteID) {
+                database.splice(i, 1);
+            };
+        };
+        
+        fs.writeFile("./db/db.json", JSON.stringify(database), (err) => {
+            if (err) throw err;
+            console.log("Note has been deleted");
+        });
+
+        return res.json(database);
+    });
+
+    //res.send("Got a DELETE request!")
+})
+
+
+app.listen(PORT, function() {
+    console.log("App listening on Port " + PORT);
 });
